6f3c7bc777e0351478df7efd22da96ce1f0f6f5f
iamangelmp <alexis.mplt@outlook.es>
2023-03-14 23:45:31 -0600

    adding fields validator using useState

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,23 @@
 import { useState, useEffect} from 'react';
 
 function Form() {
-  const [nombre, setNombre] = useState('Alexis');
+  const [nombreMascota, setNombreMascota] = useState('');
+  const [nombreProp, setNombreProp] = useState('');
+  const [email, setEmail] = useState('');
+  const [fecha, setFecha] = useState('');
+  const [sintomas, setSintomas] = useState('');
+  const [error, setErrores] = useState(false);
 
-  console.log(nombre);
+  function handleSubmit(e){
+    e.preventDefault();
+    if([nombreMascota,nombreProp,email,fecha,sintomas].includes('')){
+      console.log('revisa que el formulario este completo');
+      return setErrores(true);
+    }
+
+    setErrores(false);
+  }
+
   return (
     <div className="md:w-1/2 lg:w-2/5">
       <h2 className="font-black text-3xl text-center">Seguimiento pacientes</h2>
@@ -13,7 +27,11 @@ function Form() {
         <span className="text-indigo-600 font-bold">Administralos</span>
       </p>
 
-      <form action="" className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+      <form onSubmit={handleSubmit} className="bg-white shadow-md rounded-lg py-10 px-5 mb-10 mx-5">
+        {error && (<div className='bg-red-800 text-white text-center p-4 mb-4 rounded-md'>
+          <p> Todos los campos son obligatorios</p>
+        </div>)}
+
         <div className="mb-5">
           <label htmlFor="nombre_mascota" className="block text-gray-700 font-bold uppercase">
             Nombre Mascota
@@ -23,6 +41,9 @@ function Form() {
             type="text"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
             placeholder="Nombre de la Mascota"
+            value={nombreMascota}
+            onChange={(e)=>{setNombreMascota(e.target.value)}}
+            
           />
         </div>
 
@@ -35,6 +56,8 @@ function Form() {
             type="text"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
             placeholder="Nombre del Propietario"
+            value={nombreProp}
+            onChange={(e)=>{setNombreProp(e.target.value)}}
           />
         </div>
 
@@ -47,6 +70,8 @@ function Form() {
             type="email"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
             placeholder="Email de Contacto"
+            value={email}
+            onChange={(e)=>{setEmail(e.target.value)}}
           />
         </div>
 
@@ -58,6 +83,8 @@ function Form() {
             id="fecha_alta"
             type="date"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            value={fecha}
+            onChange={(e)=>{setFecha(e.target.value)}}
           />
         </div>
 
@@ -66,7 +93,10 @@ function Form() {
             Sintomas
           </label>
           <textarea id="sintomas" rows="5" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-            placeholder="Describe los sintomas"></textarea>
+            placeholder="Describe los sintomas"
+            value={sintomas}
+            onChange={(e=>{setSintomas(e.target.value)})}
+          />
         </div>
 
         <input type="submit" className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-all" id="" />
--- src/components/PatienrsList.jsx
+++ src/components/PatienrsList.jsx
@@ -2,7 +2,7 @@ import Patient from "./Patient";
 
 function PatienrsList() {
   return (
-    <div className="lg:w-3/5 md:w-1/2 h-screen overflow-y-scroll ">
+    <div className="mx-3 lg:w-3/5 md:w-1/2 h-screen overflow-y-scroll ">
         <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
         
         <p className="text-lg mt-5 text-center mb-10">
